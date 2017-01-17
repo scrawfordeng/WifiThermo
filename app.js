5179357d46f85ec4bbc636f5bd80e2d937f58089
Scott Crawford <scrawford.eng@gmail.com>
2017-01-16 22:08:28 -0500

    Proof of concept with PI internal temp streaming

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 var express = require('express');
-var Mcp3008 = require('./mcp3008.js');
-
+var http = require('http');
 var app = express();
-
-var adc = new Mcp3008();
-var channel = 0;
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+var temp = require('pi-temperature');
 
 
 app.get('/', function(req, res){
-  adc.read(channel, function(value){
-    if(value){
-      res.send(value);
-    } else{
-      res.send('No value');
-    }
-
-  });
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function(){
+io.on('connection', function(socket){
+  setInterval(function(){
+    temp.measure(function(temp){
+      var ftemp = temp * 1.8 + 32;
+      io.emit('Temp', ftemp);
+    });
+  }, 500);
+});
+
+server.listen(3000, function(){
   console.log('listening on port 3000...')
 });
