093424dfc637b6e8d440af5da4ce2a07590ccfe8
Scott Crawford <scrawford.eng@gmail.com>
2017-01-16 22:57:34 -0500

    Added start and stop polling functions

--- app.js
+++ app.js
@@ -10,13 +10,31 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var ival = null;
+var sockets = 0;
+
 io.on('connection', function(socket){
-  setInterval(function(){
-    temp.measure(function(temp){
-      var ftemp = temp * 1.8 + 32;
-      io.emit('Temp', ftemp);
-    });
-  }, 500);
+  
+  sockets++;
+  
+  if(ival === null){
+    console.log('Started polling...');
+    ival = setInterval(function(){
+      temp.measure(function(temp){
+        var ftemp = temp * 1.8 + 32;
+        io.emit('Temp', ftemp);
+      });
+    }, 500);
+  };
+
+  socket.on('disconnect', function(){
+    sockets--;
+    if(sockets < 1){
+      clearInterval(ival);
+      ival == null;
+      console.log('Stopped polling...')
+    };
+  });
 });
 
 server.listen(3000, function(){
